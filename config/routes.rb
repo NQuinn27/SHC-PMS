05237aeb4db0007e93c085c6e3675f98be5e46b3
Niall Quinn <niall@niallquinn.me>
2017-03-25 11:39:01 +0000

    Styled all login/signup pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     confirmations:  'users/confirmations',
     passwords:  'users/passwords',
-    unlocks: 'users/unlocks'
+    unlocks: 'users/unlocks',
+    registrations: 'users/registrations'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 

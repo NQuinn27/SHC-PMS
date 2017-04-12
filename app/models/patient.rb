076c8b695a94cbfe06ed511a51558d75e8c23601
Niall Quinn <niall@niallquinn.me>
2017-04-12 10:26:18 +0100

    assets compile

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,7 +2,7 @@ class Patient < ApplicationRecord
   has_many :patient_conditions, dependent: :nullify
 
   def self.search(search)
-    where("first_name LIKE ? OR last_name LIKE ?", "%#{search}%", "%#{search}%")
+    where("first_name LIKE ? OR last_name LIKE ?", "%#{search.downcase}%", "%#{search.downcase}%")
   end
 
 end
